efe58d72148975080ec904a7e671731d0f02127c
demchenkoalexeyv <demchenko.alexey.v@gmail.com>
2019-07-12 11:54:11 +0300

    CPC-05 Implementing save logic for VetServiceMap

--- petclinic-data/src/main/java/org/ademchenko/custompetclinic/services/map/VetServiceMap.java
+++ petclinic-data/src/main/java/org/ademchenko/custompetclinic/services/map/VetServiceMap.java
@@ -1,6 +1,8 @@
 package org.ademchenko.custompetclinic.services.map;
 
+import org.ademchenko.custompetclinic.model.Speciality;
 import org.ademchenko.custompetclinic.model.Vet;
+import org.ademchenko.custompetclinic.services.SpecialtyService;
 import org.ademchenko.custompetclinic.services.VetService;
 import org.springframework.stereotype.Service;
 
@@ -8,6 +10,12 @@ import java.util.Set;
 
 @Service
 public class VetServiceMap extends AbstractMapService<Vet, Long> implements VetService {
+
+    private final SpecialtyService specialtyService;
+
+    public VetServiceMap(SpecialtyService specialtyService) {
+        this.specialtyService = specialtyService;
+    }
 
     @Override
     public Set<Vet> findAll() {
@@ -26,6 +34,15 @@ public class VetServiceMap extends AbstractMapService<Vet, Long> implements VetS
 
     @Override
     public Vet save(Vet object) {
+
+        if(object.getSpecialities().size() > 0){
+            object.getSpecialities().forEach(speciality -> {
+                if(speciality.getId() == null){
+                    Speciality savedSpeciality = specialtyService.save(speciality);
+                    speciality.setId(savedSpeciality.getId());
+                }
+            });
+        }
         return super.save(object);
     }
 
